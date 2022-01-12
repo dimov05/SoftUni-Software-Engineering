303014d823e927929ac79f6e053b64eb0536ebf5
dimov05 <ddimov.1999@gmail.com>
2022-01-12 18:56:53 +0200

    Exercising Introduction to Classes :)

---
+++ src/ExerciseDefiningClasses/RawData/Cargo.java
@@ -0,0 +1,11 @@
+package ExerciseDefiningClasses.RawData;
+
+public class Cargo {
+    int weight;
+    String type;
+
+    public Cargo(int weight, String type) {
+        this.weight = weight;
+        this.type = type;
+    }
+}
