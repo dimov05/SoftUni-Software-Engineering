90c42cd705787d16cdaa48570946e553dcb05e28
dimov05 <ddimov.1999@gmail.com>
2022-01-12 18:56:15 +0200

    Exercising Introduction to Classes :)

---
+++ src/ExerciseDefiningClasses/RawData/Engine.java
@@ -0,0 +1,11 @@
+package ExerciseDefiningClasses.RawData;
+
+public class Engine {
+    int speed;
+    int power;
+
+    public Engine(int speed, int power) {
+        this.speed = speed;
+        this.power = power;
+    }
+}
