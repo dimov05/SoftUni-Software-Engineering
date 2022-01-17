7daf8c43241e41543357c4d961bb53cc2ef96019
dimov05 <ddimov.1999@gmail.com>
2022-01-17 11:27:26 +0200

    Learning Generics :)

---
+++ src/Generics/ArrayCreator/Main3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Integer[] array = ArrayCreator.create(13,13);
+        String[] strings =
+                ArrayCreator
+                        .create(String.class,13,"Java");
+
+    }
+}
