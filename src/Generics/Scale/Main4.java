4e372f17a45bdb025f798ff425c912be5322807b
dimov05 <ddimov.1999@gmail.com>
2022-01-18 10:32:43 +0200

    Generics :)

---
+++ src/Generics/Scale/Main4.java
@@ -0,0 +1,6 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Scale<String> stringScale = new Scale<>("A","A");
+        System.out.println(stringScale.getHeavier());
+    }
+}
