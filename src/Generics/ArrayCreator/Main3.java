21748185c834fda1f242017ad39a5c3b98f88e6d
dimov05 <ddimov.1999@gmail.com>
2022-01-18 10:33:43 +0200

    Generics :)

---
+++ src/Generics/ArrayCreator/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args) {
+        Integer[] array = ArrayCreator.create(13,13);
+        String[] strings =
+                ArrayCreator
+                        .create(String.class,13,"Java");
+
+    }
+}
