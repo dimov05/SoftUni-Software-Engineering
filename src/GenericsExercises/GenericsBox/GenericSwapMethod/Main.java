d985cb6a67688d720aaaabcaadd7d00be180e2aa
dimov05 <ddimov.1999@gmail.com>
2022-01-19 11:44:33 +0200

    Exercising Generics :)

---
+++ src/GenericsExercises/GenericsBox/GenericSwapMethod/Main.java
@@ -0,0 +1,20 @@
+package GenericsExercises.GenericsBox.GenericSwapMethod;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Box<Integer> box = new Box<>();
+        for (int i = 0; i < n; i++) {
+            int input = Integer.parseInt(scanner.nextLine());
+            box.addValue(input);
+        }
+        int firstInd = scanner.nextInt();
+        int secondInd = scanner.nextInt();
+
+        box.swap(firstInd, secondInd);
+        System.out.println(box);
+    }
+}
