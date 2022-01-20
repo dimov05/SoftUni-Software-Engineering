d6e44e3ef460f208c1d62bdba446cad4e4ac92fa
dimov05 <ddimov.1999@gmail.com>
2022-01-20 14:22:10 +0200

    Exercising Generics :)

---
+++ src/GenericsExercises/GenericsBox/GenericSwapMethod/Box.java
@@ -0,0 +1,37 @@
+package GenericsExercises.GenericsBox.GenericSwapMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> values;
+
+    public Box() {
+        this.values = new ArrayList<>();
+    }
+
+    public void addValue(T value) {
+        this.values.add(value);
+    }
+
+    public void swap(int index1, int index2) {
+        T firstEl = this.values.get(index1);
+        T secondEl = this.values.get(index2);
+        this.values.set(index1, secondEl);
+        this.values.set(index2, firstEl);
+
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : this.values) {
+            sb.append(String.format("%s: %s",
+                    value.getClass().getName(),
+                    value));
+            sb.append(System.lineSeparator());
+
+        }
+        return sb.toString().trim();
+    }
+}
