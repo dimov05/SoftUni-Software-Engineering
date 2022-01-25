4d71cb6a87731656b314f579847ee496974c1e59
dimov05 <ddimov.1999@gmail.com>
2022-01-25 12:00:32 +0200

    Exercising Iterators and Comparators :)

---
+++ src/ExerciseIteratorsAndComparators/StrategyPattern/Person.java
@@ -0,0 +1,27 @@
+package ExerciseIteratorsAndComparators.StrategyPattern;
+
+public class Person {
+    private String name;
+    private int age;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", this.name, this.age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+}
